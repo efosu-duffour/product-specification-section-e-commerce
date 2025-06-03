95d783cbc9ca803ebedb3f2d9cc0a56a843b63e5
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-06-03 03:55:42 +0000

    Fix: Configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/product-specification-secton-e-commerce/'
+    base: '/product-specification-section-e-commerce/'
 })      
